719957bce91c282bc478988a0e68ac05daaae86d
sunnny4u <sunnycse1995@gmail.com>
2020-03-09 22:26:02 +0600

    dbms-part-1

---
+++ StudentManagement/src/com/codespathshala/studentInfo.java
@@ -0,0 +1,14 @@
+package com.codespathshala;
+
+import javax.swing.*;
+
+public class studentInfo {
+    private JPanel panelMain;
+    private JPanel panelTop;
+    private JPanel panelLeft;
+    private JPanel panelRight;
+    private JLabel title;
+    private JPanel panelLabel;
+    private JPanel panelInputbox;
+    private JTable table1;
+}
