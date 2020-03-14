43ddbed967105d2a094fb923aa5ef6533fc3f41e
sunnny4u <sunnycse1995@gmail.com>
2020-03-14 23:09:16 +0600

    Design complete

--- StudentManagement/src/com/codespathshala/studentInfo.java
+++ StudentManagement/src/com/codespathshala/studentInfo.java
@@ -11,4 +11,21 @@ public class studentInfo {
     private JPanel panelLabel;
     private JPanel panelInputbox;
     private JTable table1;
+    private JLabel labelName;
+    private JLabel labelID;
+    private JLabel labelCourse;
+    private JTextField fieldName;
+    private JTextField fieldId;
+    private JTextField fieldCourse;
+    private JButton btnAdd;
+    private JButton btnEdit;
+    private JButton btnDelete;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Record");
+        frame.setContentPane(new studentInfo().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
